4e8e6a348ce2364f51d7106279c84445da4818df
evame94 <katerinka.melnichuk@gmail.com>
2022-06-20 14:11:18 +0200

    Whole new exercise

---
+++ resources/views/about/about-us.blade.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <nav>
+    <a href="{{route("index")}}">Home</a>
+    <a href="{{route("about")}}">About</a>
+  </nav>
+  <h1>About us</h1>
+  <p>My name is Ekaterina</p>
+  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::view("/", "index/index")->name("index");
+Route::view("/about-us", "about/about-us")->name("about");
